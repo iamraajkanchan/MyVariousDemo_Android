f4668f05f9681a7bfa09f12fa99c566bc4e7b8b3
Raj K <raj.k@mobicule.com>
2023-04-30 16:36:40 +0530

    New constants are added in Constants.kt

--- app/src/main/java/com/example/filesystemdemo/utilities/Constants.kt
+++ app/src/main/java/com/example/filesystemdemo/utilities/Constants.kt
@@ -3,3 +3,6 @@ package com.example.filesystemdemo.utilities
 const val TAG: String = "FileSystemDemo"
 const val ARG_FILE_NAME: String = "file name argument"
 const val ARG_FOLDER_NAME: String = "folder name argument"
+const val EMPTY_FILE_ERROR: String = "File is empty!!!"
+const val FILE_NOT_EXIST_ERROR: String = "File doesn't exist!!!"
+const val FOLDER_NOT_EXIST_ERROR: String = "Folder doesn't exist!!!"
