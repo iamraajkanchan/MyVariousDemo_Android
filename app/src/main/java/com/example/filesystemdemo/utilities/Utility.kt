b7d6a7917c83117e57c36fbc4fdd5a503729782f
Raj K <raj.k@mobicule.com>
2023-05-06 11:34:14 +0530

    getCurrentTime() method is added in Utility.kt

--- app/src/main/java/com/example/filesystemdemo/utilities/Utility.kt
+++ app/src/main/java/com/example/filesystemdemo/utilities/Utility.kt
@@ -1,11 +1,25 @@
 package com.example.filesystemdemo.utilities
 
+import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.snackbar.Snackbar
+import java.text.SimpleDateFormat
+import java.util.*
 
-class Utility(private val view: CoordinatorLayout) {
+class Utility() {
+
+    private lateinit var view: CoordinatorLayout
+
+    constructor(view: CoordinatorLayout) : this() {
+        this.view = view
+    }
 
     fun showSnackBar(message: String) {
         Snackbar.make(view, message, Snackbar.LENGTH_LONG).show()
+    }
+
+    fun getCurrentTime(): String {
+        val timeFormat = SimpleDateFormat("dd-MM-yy HH:mm:ss", Locale.ENGLISH)
+        return timeFormat.format(Calendar.getInstance().time)
     }
 }
