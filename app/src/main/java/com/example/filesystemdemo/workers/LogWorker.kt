d4a50af1f423293a363377142ed3bd77966163a3
Raj K <raj.k@mobicule.com>
2023-05-06 12:31:35 +0530

    LogWorker.kt is modified.

--- app/src/main/java/com/example/filesystemdemo/workers/LogWorker.kt
+++ app/src/main/java/com/example/filesystemdemo/workers/LogWorker.kt
@@ -4,18 +4,13 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Bitmap.createBitmap
-import android.graphics.Canvas
 import android.os.Build
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.app.NotificationCompat
 import androidx.work.CoroutineWorker
 import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
 import com.example.filesystemdemo.R
-import java.io.File
-import java.io.FileWriter
+import com.example.filesystemdemo.utilities.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -23,6 +18,8 @@ private const val LOG_NOTIFICATION_ID: Int = 0
 
 class LogWorker(private val context: Context, params: WorkerParameters) :
     CoroutineWorker(context, params) {
+
+    private val utility: Utility by lazy { Utility(context) }
 
     override suspend fun getForegroundInfo(): ForegroundInfo {
         return ForegroundInfo(LOG_NOTIFICATION_ID, Notification())
@@ -33,13 +30,6 @@ class LogWorker(private val context: Context, params: WorkerParameters) :
             applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(LOG_NOTIFICATION_ID, createNotification(notificationManager))
         return Result.success()
-    }
-
-    private fun writeInFile(file: File) {
-        val writer: FileWriter = FileWriter(file, true)
-        writer.append(getCurrentTime()).append(" ").append("Logging from WorkManager").append("\n")
-        writer.flush()
-        writer.close()
     }
 
     private fun getCurrentTime(): String {
@@ -60,46 +50,28 @@ class LogWorker(private val context: Context, params: WorkerParameters) :
                 }
                 val builder: Notification.Builder =
                     Notification.Builder(context, NOTIFICATION_CHANNEL).apply {
-                        setLargeIcon(vectorToBitmap(R.drawable.ic_launcher_foreground))
-                        setSmallIcon(R.drawable.icon_delete)
-                        setContentTitle("FileSystem Log Service")
-                        setContentText("${getCurrentTime()} :: Logging From WorkManager")
-                        style = Notification.BigTextStyle().bigText("")
                         setChannelId(NOTIFICATION_CHANNEL)
+                        setLargeIcon(utility.vectorToBitmap(R.drawable.ic_launcher_foreground))
+                        setSmallIcon(R.drawable.icon_delete)
+                        setContentTitle(NOTIFICATION_TITLE)
+                        setContentText("${getCurrentTime()} :: Logging From WorkManager")
                         setAutoCancel(false)
+                        style = Notification.BigTextStyle().bigText("")
                     }
                 notificationManager.createNotificationChannel(channel)
                 builder.build()
             } else {
                 val builder = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL).apply {
-                    setLargeIcon(vectorToBitmap(R.drawable.ic_launcher_foreground))
+                    setChannelId(NOTIFICATION_CHANNEL)
+                    setLargeIcon(utility.vectorToBitmap(R.drawable.ic_launcher_foreground))
                     setSmallIcon(R.drawable.icon_delete)
                     setContentTitle("Log Service")
                     setContentText("${getCurrentTime()} :: Logging From WorkManager")
-                    setChannelId(NOTIFICATION_CHANNEL)
                     setAutoCancel(false)
                 }
                 builder.build()
             }
         return notification
-    }
-
-    companion object {
-        const val NOTIFICATION_ID = "FileSystem_notification_id"
-        const val NOTIFICATION_NAME = "FileSystem"
-        const val NOTIFICATION_CHANNEL = "FileSystem_channel_01"
-        const val NOTIFICATION_WORK = "FileSystem_notification_work"
-    }
-
-    fun vectorToBitmap(drawableId: Int): Bitmap? {
-        val drawable = AppCompatResources.getDrawable(context, drawableId) ?: return null
-        val bitmap = createBitmap(
-            drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888
-        ) ?: return null
-        val canvas = Canvas(bitmap)
-        drawable.setBounds(0, 0, canvas.width, canvas.height)
-        drawable.draw(canvas)
-        return bitmap
     }
 
 }
