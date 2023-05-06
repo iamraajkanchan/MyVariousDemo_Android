8490790a09e4e8ba2b2be8eff3348340cbea3929
Raj K <raj.k@mobicule.com>
2023-05-06 12:30:05 +0530

    vectorToBitmap() method is added in Utility.kt

--- app/src/main/java/com/example/filesystemdemo/utilities/Utility.kt
+++ app/src/main/java/com/example/filesystemdemo/utilities/Utility.kt
@@ -1,16 +1,19 @@
 package com.example.filesystemdemo.utilities
 
-import android.view.View
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.snackbar.Snackbar
 import java.text.SimpleDateFormat
 import java.util.*
 
-class Utility() {
+class Utility(private val context: Context) {
 
     private lateinit var view: CoordinatorLayout
 
-    constructor(view: CoordinatorLayout) : this() {
+    constructor(context: Context, view: CoordinatorLayout) : this(context) {
         this.view = view
     }
 
@@ -21,5 +24,16 @@ class Utility() {
     fun getCurrentTime(): String {
         val timeFormat = SimpleDateFormat("dd-MM-yy HH:mm:ss", Locale.ENGLISH)
         return timeFormat.format(Calendar.getInstance().time)
+    }
+
+    fun vectorToBitmap(drawableId: Int): Bitmap? {
+        val drawable = AppCompatResources.getDrawable(context, drawableId) ?: return null
+        val bitmap = Bitmap.createBitmap(
+            drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888
+        ) ?: return null
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+        return bitmap
     }
 }
